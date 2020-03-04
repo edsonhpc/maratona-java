65bf96724ab50f1ef30eec00878a45cc4de7ec9d
Edson Cavalcanti <edson.horacavalcanti@gmail.com>
2020-03-04 07:18:03 -0300

    Aula: Tipos primitivos, convenção de código e comentários

---
+++ JavaOnline/src/ImprimindoVariaveis.java
@@ -0,0 +1,8 @@
+public class ImprimindoVariaveis {
+
+    public static void main (String[] args){
+        int idadeDosPaisParaCadastro = 10;
+        System.out.println("Idade é: "+ idadeDosPaisParaCadastro);
+
+    }
+}
